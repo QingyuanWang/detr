fd7ec8eab06bb7d3cc9074efe4644aba415a4065
qingyuan <qingyuan.wang@ucdconnect.ie>
2020-07-12 19:33:30 +0800

    fix

--- models/matcher.py
+++ models/matcher.py
@@ -79,12 +79,10 @@ class HungarianMatcher(nn.Module):
         C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + self.cost_giou * cost_giou
         C = C.view(bs, num_queries, -1).cpu()
 
-        sizes = [len(v["boxes"]) for v in targets]
-        sizes_ = []
-        for i in range(self.multiple_match):
-            sizes_.extend(sizes)
-        indices = [linear_sum_assignment(c[i % bs]) for i, c in enumerate(C.split(sizes_, -1))]
-        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j % len(targets), dtype=torch.int64)) for i, j in indices]
+        sizes = [len(v["boxes"]) * self.multiple_match for v in targets]
+        indices = [linear_sum_assignment(c[i % bs]) for i, c in enumerate(C.split(sizes, -1))]
+        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j % len(targets), dtype=torch.int64))
+                for i, j in indices]
 
 
 def build_matcher(args):
