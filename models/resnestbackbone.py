3f4731daa23e24cc596ab50e85fcb205f45de7e0
qingyuan <qingyuan.wang@ucdconnect.ie>
2020-07-28 20:50:44 +0800

    512

--- models/resnestbackbone.py
+++ models/resnestbackbone.py
@@ -16,11 +16,6 @@ def resnest_fpn_backbone(pretrained, norm_layer=misc_nn_ops.FrozenBatchNorm2d, t
     backbone = resnest101e(pretrained=pretrained)
     # select layers that wont be frozen
     assert trainable_layers <= 5 and trainable_layers >= 0
-    layers_to_train = ['layer4', 'layer3', 'layer2', 'layer1', 'conv1'][:trainable_layers]
-    # freeze layers only if pretrained backbone is used
-    for name, parameter in backbone.named_parameters():
-        if all([not name.startswith(layer) for layer in layers_to_train]):
-            parameter.requires_grad_(False)
     return_layers = {'layer1': '0', 'layer2': '1', 'layer3': '2', 'layer4': '3'}
     in_channels_stage2 = backbone.inplanes // 8
     in_channels_list = [
@@ -36,13 +31,11 @@ def resnest_fpn_backbone(pretrained, norm_layer=misc_nn_ops.FrozenBatchNorm2d, t
 class ResnestBackBone(nn.Module):
     def __init__(self):
         super(ResnestBackBone, self).__init__()
-        self.resnest = resnest_fpn_backbone(pretrained=True)
-        # self.weights = nn.Parameter(torch.ones(4))
+        self.resnest = resnest_fpn_backbone(pretrained=False)
 
-        self.p2_downsample = nn.MaxPool2d(kernel_size=2)
-        self.p1_downsample = nn.MaxPool2d(kernel_size=4)
-        self.p0_downsample = nn.MaxPool2d(kernel_size=8)
-        # self.p4_downsample = nn.MaxPool2d(kernel_size=2)
+        self.p0_downsample = nn.MaxPool2d(kernel_size=4)
+        self.p1_downsample = nn.MaxPool2d(kernel_size=2)
+        self.p3_upsample = nn.Upsample(scale_factor=2, mode='nearest')
 
         self.position_encoding = PositionEmbeddingSine(128, normalize=True)
         self.freeze_bn()
@@ -55,8 +48,8 @@ class ResnestBackBone(nn.Module):
 
         features = torch.cat([
             self.p0_downsample(resnest_out['0']),
-            self.p1_downsample(resnest_out['1']),
-            self.p2_downsample(resnest_out['2']), resnest_out['3']
+            self.p1_downsample(resnest_out['1']), resnest_out['2'],
+            self.p3_upsample(resnest_out['3'])
         ], 1)
 
         features = nested_tensor_from_tensor_list(features)
